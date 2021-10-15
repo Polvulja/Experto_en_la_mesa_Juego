75bf64e10932b1b69e2e390ee7d87294b73fe01f
Polvulja <67106904+Polvulja@users.noreply.github.com>
2021-10-15 20:09:52 -0300

    Add files via upload

---
+++ Tahures/Inicio.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <meta charset="uft-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie-edge">
+        <link rel="stylesheet" href="../css/inicio.css" type="text/css" media="all">
+        <link rel="stylesheet" href="../css/predefenido.css" type="text/css" media="all">
+        <title>Bienvenido || Experto en la Mesa de Juego</title>
+    </head>
+    <body>
+      <div id="sidemenu" class="menu_collapsed">
+      <!--header-->
+          <div id="header">
+              <div id="title"></div>
+              <!--profile-->
+              <!--items-->
+          </div>
+      </div>  
+    </body>
+</html>
